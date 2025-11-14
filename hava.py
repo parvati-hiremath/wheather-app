7d818963eccdf84c737a624a029fedc3312b6c08
parvati-hiremath <parvatihiremath2024@outlook.com>
2025-11-14 11:47:30 +0000

    feat:added error handling

--- hava.py
+++ hava.py
@@ -1,6 +1,29 @@
-def main():
-    city = input("Enter city: ")
-    print(f"Enter city: {city}")
+import requests
 
-if __name__  == "__name__":
+API_key = "05096f46373cd41c68d2ad49c22d4908"
+URL = "https://api.openweathermap.org/data/2.5/weather"
+
+def main():
+    city = input("Enter city: ").strip()
+    if not city:
+        print("Error:City name cannot be empty.")
+        return
+
+    try:
+        params = {"q":city,"appid":API_key, "units":"metric"}
+        resp = requests.get(URL, params = params)
+        resp.raise_for_status() #throws 404/500
+        data = resp.json() 
+        print(f"{city} temperature: {data['main']['temp']} °c")
+
+    except requests.exceptions.HTTPError as h:
+        print("API error:{h}")
+
+    except KeyError:
+        print("Error: Expected data from API not received.")
+
+    except Exception as e:
+        print(f"Unknown error:{e}")
+
+if __name__  == "__main__":
     main()
